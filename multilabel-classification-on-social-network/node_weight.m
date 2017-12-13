897d2b314a30c7358c90ca94579728f2264f3811
tian liu <tianliu@tiandeiMac.local>
2017-12-13 17:11:26 -0600

    delete

--- multilabel-classification-on-social-network/node_weight.m
+++
@@ -1,3 +0,0 @@
-function Sim = node_weight(Net,IDX1,IDX2)
-Sim = Net(IDX1,IDX2)/sum(Net(IDX1,IDX2));
-end
--- multilabel-classification-on-social-network/scalable_kmeans.m
+++
@@ -1,1 +0,0 @@
-function [idx, clustersize, numiter, objective] = scalable_kmeans(data, k, maxIter, tol)
